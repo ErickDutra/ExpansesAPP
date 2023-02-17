71b21d28481dabd1457623074fa802bab06dfedf
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2023-02-16 21:22:35 -0300

    melhorias na amostra de gastos

--- expanses/lib/main.dart
+++ expanses/lib/main.dart
@@ -1,3 +1,4 @@
+import './models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpansesApp());
@@ -10,25 +11,87 @@ class ExpansesApp extends StatelessWidget {
 }
 
 class MyHomePag extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'yeezy',
+      value: 320.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'jordan',
+      value: 990.90,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Column(children: <Widget>[
-        Container(
-          width: double.infinity,
-          child: Card(
-            color: Colors.blue,
-            child: Text('grafico'),
-            elevation: 5,
-          ),
-        ),
-        Card(
-          child: Text('lista de transações'),
-        )
-      ]),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('grafico'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(
+                              color: Colors.blue,
+                              width: 2,
+                            )),
+                        padding: EdgeInsets.all(10),
+                        child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.green)),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            tr.date.toString(),
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Color.fromARGB(255, 99, 98, 98),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ]),
     );
   }
 }
